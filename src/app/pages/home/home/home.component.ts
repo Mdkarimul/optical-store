5319345571e3c6beedecf9b1e68ebb08ca1b9b34
karimul <7mdkarimul@gmail.com>
2024-08-13 17:47:48 +0530

    add appointment form

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from '../header/header.component';
 import { ServicesComponent } from '../services/services.component';
 import { FooterComponent } from '../footer/footer.component';
 import { AboutComponent } from '../about/about.component';
+import { AppointmentComponent } from "../appointment/appointment.component";
+import { BlogComponent } from "../blog/blog.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, HeaderComponent, ServicesComponent, FooterComponent, AboutComponent],
+  imports: [NavComponent, HeaderComponent, ServicesComponent, FooterComponent, AboutComponent, AppointmentComponent, BlogComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
